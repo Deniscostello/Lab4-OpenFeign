966ce80ed08e7e649efd30ee4553014594318690
Denis Costello <G00396309@atu.ie>
2023-10-10 15:47:12 +0100

    Created RegistrationServiceClient interface

--- src/main/java/ie/atu/userservice/RegistrationServiceClient.java
+++ src/main/java/ie/atu/userservice/RegistrationServiceClient.java
@@ -1,4 +1,11 @@
 package ie.atu.userservice;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "registration-service", url = "http://localhost:8082")
 public interface RegistrationServiceClient {
+    @PostMapping("/register")
+    String SomeDetails(@RequestBody UserDetails userDetails);
 }
