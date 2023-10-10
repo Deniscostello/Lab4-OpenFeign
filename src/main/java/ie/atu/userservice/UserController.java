f7fac893e9318961742909bcd9060f81504f614f
Denis Costello <G00396309@atu.ie>
2023-10-10 16:31:41 +0100

    Added  Registration Endpoint:

--- src/main/java/ie/atu/userservice/UserController.java
+++ src/main/java/ie/atu/userservice/UserController.java
@@ -1,6 +1,11 @@
 package ie.atu.userservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserController {
     private RegistrationServiceClient registrationServiceClient;
@@ -8,6 +13,15 @@ public class UserController {
     @Autowired
     public UserController(RegistrationServiceClient registrationServiceClient){
         this.registrationServiceClient=registrationServiceClient;
+    }
+
+    @PostMapping("/confirm-and-register")
+    public Map<String, String> confirmAndRegister(@RequestBody UserDetails userDetails){
+        String confirm=registrationServiceClient.SomeDetails(userDetails);
+        Map<String, String> responseMessage = new HashMap<>();
+        responseMessage.put("message",confirm);
+        return responseMessage;
+
     }
 
 }
