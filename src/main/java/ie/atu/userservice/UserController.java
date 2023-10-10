24916525bb688d1debf88e446da801d8d57ec3f3
Denis Costello <G00396309@atu.ie>
2023-10-10 16:12:51 +0100

    Updated Postmapping in RegistrationServiceClient

--- src/main/java/ie/atu/userservice/UserController.java
+++ src/main/java/ie/atu/userservice/UserController.java
@@ -1,4 +1,10 @@
 package ie.atu.userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserController {
+    @Autowired
+    public UserController(RegistrationServiceClient registrationserviceclient){
+        this.registrationserviceclient=registrationserviceclient;
+    }
 }
